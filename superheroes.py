116d6e7a05cbe0d3f5c549780ef01aaa560fe350
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2019-09-30 19:50:39 -0700

    add class Team

--- superheroes.py
+++ superheroes.py
@@ -1,4 +1,5 @@
 import random
+from random import randint
 
 class Ability:
     def __init__(self, name, attack_strength):
@@ -17,13 +18,32 @@ class Armor:
     def block(self):
         block = random.randint(0, self.max_block)
         return block
+
+class Weapon(Ability):
+    def attack(self):
+        weapon_attack = randint(self.attack_strength//2, self.attack_strength)
+        return weapon_attack
+
 class Hero:
     def __init__(self, name, starting_health=100):
+        self.name = name
         self.abilities = []
         self.armors = []
-        self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+        self.deaths = 0
+        self.kills = 0
+    def add_kills(self, num_kills):
+        self.kills += num_kills
+
+    def add_deaths(self, num_deaths):
+        self.deaths += num_deaths
+
+    def add_weapon(self, weapon):
+        self.abilities.append(weapon)
+
+    def add_armor(self, armor):
+        self.armors.append(armor)
 
     def current_health(self):
         return self.current_health
@@ -32,23 +52,26 @@ class Hero:
         self.abilities.append(ability)
 
     def attack(self):
+        total_attack = 0
         for ability in self.abilities:
-            total_attack = ability.attack()
-            return total_attack
+            total_attack += ability.attack()
+        return total_attack
+
     def add_armor(self, armor):
         self.armors.append(armor)
 
-    def defend(self, damage_amt):
+    def defend(self, damage_amt = 0):
+        total_block = 0
         for armor in self.armors:
             total_block = armor.block()
-            return total_block
+        return total_block
 
     def take_damage(self, damage):
         self.current_health  = self.current_health - damage
         return self.current_health
 
     def is_alive(self):
-        if self.current_health >= 0:
+        if self.current_health > 0:
             return True
         else:
             return False
@@ -58,32 +81,61 @@ class Hero:
                 opponent.take_damage(self.attack())
                 self.take_damage(opponent.attack())
                 if opponent.is_alive() == False:
+                    self.add_kills(1)
+                    opponent.add_deaths(1)
                     print(self.name + ' won')
                 else:
+                    self.add_deaths(1)
+                    self.add_kills(1)
                     print(opponent.name + ' won')
             else:
                 print("Draw!")
                 return False
-class Weapon(Ability):
-    def attack(self):
-        weapon_attack = randint(self.attack_strength//2, self.attack_strength)
-        return weapon_attack
-class Team(name):
+
+class Team:
     def __init__(self, name):
         self.name = name
         self.heroes = []
 
     def remove_hero(self, name):
-        self.heroes.pop()
+        for hero in self.heroes:
+            if name == hero.name:
+                self.heroes.remove(hero)
+        return 0
 
     def view_all_heroes(self):
-        index = 0
-        for hero in self.heroes:
-            print("{}{}".format(self.name, self.heroes))
-            index += 1
+       for hero in self.heroes:
+            print('{}'.format(hero.name))
     def add_hero(self, hero):
-        self.heroes.append()
+        self.heroes.append(hero)
 
+    def attack(self, other_team):
+        while self.heroes_alive() and other_team.heroes_alive() is True:
+            first_team = random.choice(self.heroes_alive())
+            second_team = random.choice(other_team.heroes_alive())
+            first_team.fight(second_team)
+    def heroes_alive(self):
+        heroes_alive = []
+        for hero in self.heroes:
+            if hero.is_alive is True:
+                heroes_alive.append(hero)
+            return heroes_alive
+    def revive_heroes(self, health=100):
+        for hero in self.heroes:
+            hero.health = hero.starting_health
+
+    def stats(self):
+        for hero in self.heroes:
+            print("Hero name: " + hero.name)
+            print("Kills: " + str(hero.kills))
+            print("Deaths: {} \n".format(hero.deaths))
+class Arena:
+    def __init__(self):
+        self.team_one = None
+        self.team_two = None
+    #
+    # def create_ability(self):
+    #     ability = input("()")
 
 
 
@@ -124,9 +176,13 @@ if __name__ == "__main__":
     # print(hero.is_alive())
     # hero.take_damage(15000)
     # print(hero.is_alive())
-
+    team1 = Team("Team1")
+    team2 = Team("Team2")
     hero1 = Hero("Wonder Woman")
     hero2 = Hero("Dumbledore")
+    hero3 = Hero("Goku")
+    hero4 = Hero("Vegeta")
+
     ability1 = Ability("Super Speed", 300)
     ability2 = Ability("Super Eyes", 130)
     ability3 = Ability("Wizard Wand", 80)
@@ -135,4 +191,15 @@ if __name__ == "__main__":
     hero1.add_ability(ability2)
     hero2.add_ability(ability3)
     hero2.add_ability(ability4)
-    hero1.fight(hero2)
+    hero3.add_ability(ability1)
+    hero3.add_ability(ability2)
+    hero4.add_ability(ability3)
+    hero4.add_ability(ability4)
+
+    team1.add_hero(hero1)
+    team1.add_hero(hero2)
+    team2.add_hero(hero3)
+    team2.add_hero(hero4)
+    # print(team2.heroes_alive())
+
+    team1.attack(team2)
